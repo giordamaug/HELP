78a8393b56908c9a4ec09ac68db767713318c1a8
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-02-06 08:53:41 +0100

    Update setup.py

--- help/setup.py
+++ help/setup.py
@@ -1,1 +1,23 @@
+from setuptools import setup
+
+setup(
+   name='help',
+   version='0.1.0',
+   author='Maurizio Giordano and Ilaria Granata and Lucia Maddalena',
+   author_email='maurizio.giordano@icar.cnr.it',
+   packages=['help', 'help.models', 'help.utility', help.visualization],
+   license='LICENSE.txt',
+   description='The HELP Essential Genes framework',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "numpy",
+       "tqdm",
+       "typing",
+       "pandas",
+       "supervenn",
+       "matplotlib",
+       "ipywidgets",
+       "scipy"
+   ],
+)
 
