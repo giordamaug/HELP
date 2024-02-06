4d3fb60124426374bb211dd878daa65ef6944851
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-02-06 11:20:17 +0100

    Update __init__.py

--- help/utility/__init__.py
+++ help/utility/__init__.py
@@ -1,3 +1,3 @@
-from .utility.selection import select_cell_lines, EG_by_tissues_intersect
-from .visualization.ui import helpbox
-from .visualization.plot import svenn_intesect
+from .selection import select_cell_lines, EG_by_tissues_intersect
+from ..visualization.ui import Help
+from ..visualization.plot import svenn_intesect
