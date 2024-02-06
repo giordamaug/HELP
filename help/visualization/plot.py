8c4783d0b00e02aa6d7f675f9315cb11fdf53612
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-02-06 21:28:23 +0100

    Update plot.py

--- help/visualization/plot.py
+++ help/visualization/plot.py
@@ -3,6 +3,39 @@ from typing import List
 import matplotlib.pyplot as plt
 
 def svenn_intesect(sets: List[set], labels: List[str], figsize=(10,20), fontsize=10, saveflag: bool=False) ->  None:
+    """
+    Generate a Supervenn diagram to visualize the intersection of multiple sets.
+
+    Parameters
+    ----------
+    sets : List[set]
+        List of sets to be visualized.
+    labels : List[str]
+        List of labels corresponding to each set.
+    figsize : tuple, optional
+        Figure size in inches, as a tuple (width, height) (default is (10, 20)).
+    fontsize : int, optional
+        Font size for labels (default is 10).
+    saveflag : bool, optional
+        Whether to save the generated diagram as an image (default is False).
+
+    Returns
+    -------
+    None
+
+    The function generates a Supervenn diagram using the 'supervenn' library and Matplotlib.
+    The diagram visualizes the intersection of multiple sets with labeled areas.
+
+    Example
+    -------
+    ```
+    # Usage example:
+    set1 = {1, 2, 3, 4, 5}
+    set2 = {3, 4, 5, 6, 7}
+    set3 = {5, 6, 7, 8, 9}
+    svenn_intersect([set1, set2, set3], ["Set A", "Set B", "Set C"], saveflag=True)
+    ```
+    """
     plt.figure(figsize=figsize)
     supervenn(sets, labels, widths_minmax_ratio=0.05, side_plots='right')
     plt.xlabel('no. Genes', fontsize = fontsize)
