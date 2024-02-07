fe9c359a093c9e37448aa3d4cdee88c604fcd08d
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-02-07 11:53:34 +0100

    upd

--- help/visualization/__init__.py
+++ help/visualization/__init__.py
@@ -1,2 +1,3 @@
 from ..models.labelling import Help
-from ..utility.selection import select_cell_lines, feature_assemble
+from ..utility.selection import select_cell_lines
+from ..preprocess.loaders import feature_assemble
