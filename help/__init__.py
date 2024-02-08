42061d1ca4557fbda34403ba9c7501edf4574b70
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-02-08 11:42:46 +0100

    Update __init__.py

--- help/__init__.py
+++ help/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
 from .models.labelling import Help
-from .utility.selection import select_cell_lines, EG_by_tissues_intersect
+from .utility.selection import select_cell_lines
 from .visualization.ui import Help_Dashboard
 from .visualization.plot import svenn_intesect
