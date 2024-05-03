7f69d72da8338800b946d28f0a3d86aa12577e16
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-05-03 10:47:38 +0200

    upd

--- HELPpy/__init__.py
+++ HELPpy/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
 from .models.labelling import labelling
 from .utility.selection import select_cell_lines
-from .visualization.ui import Help_Dashboard
+from .visualization.ui import pipeline
 from .visualization.plot import svenn_intesect
