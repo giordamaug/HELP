d428c3afb0bc7c7c7481c64ff47f29e4a2a613ef
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-05-23 09:35:14 +0200

    upd

--- batch/run_help_EvsNE.sh
+++ batch/run_help_EvsNE.sh
@@ -8,15 +8,15 @@ DATA=$HOME/$ROOTDIR/HELP/data
 TARGET=$HOME/$ROOTDIR/HELP/data
 SCOREDIR=$HOME/$ROOTDIR/HELP/scores
 LOGDIR=$HOME/$ROOTDIR/HELP/logs
-TISSUE=Human
+TISSUE=Brain
 PROB=EvsNE
 ALIASES="{'aE':'NE', 'sNE': 'NE'}"
 #ALIASES="{}"
 EXCLABELS=""
 LR = "-lr 0.5"
 VOTERS="-v 10"
-#LABELFILE=${TISSUE}_${METHOD}.csv
-LABELFILE=PanTissue_group_${METHOD}.csv
+LABELFILE=${TISSUE}_${METHOD}.csv
+#LABELFILE=PanTissue_group_${METHOD}.csv
 
 echo "running BIO+CC"
 echo "$CMD $PRG -i $DATA/${TISSUE}_BIO.csv $DATA/${TISSUE}_CCcfs.csv -l $TARGET/${LABELFILE} -A \"$ALIASES\" -n std -ba $VOTERS $LR $EXCLABELS -j -1 -B  -o $LOGDIR/log_batch_${METHOD}_${TISSUE}_${PROB}.txt -s $SCOREDIR/score_${METHOD}_${TISSUE}_${PROB}_biocc.csv" | bash
