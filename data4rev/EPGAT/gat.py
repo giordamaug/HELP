00419fc6ebf9cbe92d7b83d849ba1c15af0f86c6
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-05-25 18:36:31 +0200

    Update gat.py

--- data4rev/EPGAT/gat.py
+++ data4rev/EPGAT/gat.py
@@ -26,7 +26,7 @@ gat_human = {
 GATparams = {'KIDNEY': {'linear_layer': 256, 
                      #'n_layers': 2, 
                      'h_feats': [16, 8, 1], 
-                     'heads': [2, 4, 1], 
+                     'heads': [2, 4, 8], 
                      'lr': 0.0010633766921485904, 
                      'weight_decay': 3.905559247728033e-05, 
                      'dropout': 0.3655805198848601,
