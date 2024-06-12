29eb98e18b71d4497bd087953dc27b0517f8e027
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-06-12 15:44:09 +0200

    upd

--- batch/run_help_EvsNE.sh
+++ batch/run_help_EvsNE.sh
@@ -8,15 +8,15 @@ DATA=$HOME/$ROOTDIR/HELP/data
 TARGET=$HOME/$ROOTDIR/HELP/data
 SCOREDIR=$HOME/$ROOTDIR/HELP/data4rev/scoresfinal
 LOGDIR=$HOME/$ROOTDIR/HELP/data4rev/logsfinal
-TISSUE=Human
+TISSUE=Lung
 PROB=EvsNE
-#ALIASES="{'aE':0, 'sNE': 0, 'E': 1}"
-ALIASES="{'NE':0, 'E': 1}"
+ALIASES="{'aE':0, 'sNE': 0, 'E': 1}"
+#ALIASES="{'NE':0, 'E': 1}"
 LR="-lr 0.1" # 0.0945 from optuna
 VOTERS="-v 13"
 ESTIMATORS="-e 200"
-#LABELFILE=${TISSUE}_${METHOD}.csv
-LABELFILE=PanTissue_group_HELP.csv
+LABELFILE=${TISSUE}_${METHOD}.csv
+#LABELFILE=PanTissue_group_HELP.csv
 
 echo "running CC"
 echo "$CMD $PRG -i $DATA/${TISSUE}_CCcfs.csv -l $TARGET/${LABELFILE} -A \"$ALIASES\" -n std $VOTERS $ESTIMATORS $LR -j -1 -B  -o $LOGDIR/log_batch_${METHOD}_${TISSUE}_${PROB}.txt -s $SCOREDIR/score_${METHOD}_${TISSUE}_${PROB}_cc.csv" | bash
@@ -26,6 +26,8 @@ echo "running N2V"
 echo "$CMD $PRG -i $DATA/${TISSUE}_EmbN2V_128.csv -l $TARGET/${LABELFILE} -A \"$ALIASES\" -n std $VOTERS $ESTIMATORS $LR -j -1 -B  -o $LOGDIR/log_batch_${METHOD}_${TISSUE}_${PROB}.txt -s $SCOREDIR/score_${METHOD}_${TISSUE}_${PROB}_n2v.csv" | bash
 #echo "running BIO+N2V"
 #echo "$CMD $PRG -i $DATA/${TISSUE}_BIO.csv $DATA/${TISSUE}_EmbN2V_128.csv -l $TARGET/${LABELFILE} -A \"$ALIASES\" -n std $VOTERS $ESTIMATORS $LR -j -1 -B  -o $LOGDIR/log_batch_${METHOD}_${TISSUE}_${PROB}.txt -s $SCOREDIR/score_${METHOD}_${TISSUE}_${PROB}_bion2v.csv" | bash
+#echo "running CCBeder"
+#echo "$CMD $PRG -i $DATA/${TISSUE}_CCBeder.csv -l $TARGET/${LABELFILE} -A \"$ALIASES\" -n std $VOTERS $ESTIMATORS $LR -j -1 -B  -o $LOGDIR/log_batch_${METHOD}_${TISSUE}_${PROB}.txt -s $SCOREDIR/score_${METHOD}_${TISSUE}_${PROB}_ccbeder.csv" | bash
 echo "running BIO+N2V+CCcfs"
 echo "$CMD $PRG -i $DATA/${TISSUE}_BIO.csv $DATA/${TISSUE}_CCcfs.csv $DATA/${TISSUE}_EmbN2V_128.csv -l $TARGET/${LABELFILE} -A \"$ALIASES\" -n std $VOTERS $ESTIMATORS $LR -j -1 -B  -o $LOGDIR/log_batch_${METHOD}_${TISSUE}_${PROB}.txt -s $SCOREDIR/score_${METHOD}_${TISSUE}_${PROB}_bioccn2v.csv" | bash
 #echo "running BIO+N2V+CCcfs"
