99a0f6bf43c1f6709784012fb1794eaec0d8134a
Maurizio Giordano <maurizio.giordano@cnr.it>
2024-07-04 16:31:43 +0200

    upd

--- batch/run_help_EvsNE.sh
+++ batch/run_help_EvsNE.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 METHOD=HELP
-ROOTDIR=PLOS_CompBiology
+ROOTDIR=PLOS_CompBiology/HELP_branches/v2.0
+ROOTDIR2=PLOS_CompBiology/HELP_branches/v2.0
 ENV=envs/mytorch
 CMD=$HOME/miniconda3/$ENV/bin/python
 PRG=$HOME/$ROOTDIR/HELP/notebooks/EG_prediction.py
-DATA=$HOME/$ROOTDIR/HELP/data
-TARGET=$HOME/$ROOTDIR/HELP/data
-SCOREDIR=$HOME/$ROOTDIR/HELP/scores
-LOGDIR=$HOME/$ROOTDIR/HELP/logs
+DATA=$HOME/$ROOTDIR2/HELP/data
+TARGET=$HOME/$ROOTDIR2/HELP/data
+SCOREDIR=$HOME/$ROOTDIR/HELP/scoresgm
+LOGDIR=$HOME/$ROOTDIR/HELP/logsgm
 TISSUE=Kidney
 PROB=EvsNE
 ALIASES="{'aE':0, 'sNE': 0, 'E': 1}"
 #ALIASES="{'NE':0, 'E': 1}"
-LR="-lr 0.1" # 0.0945 from optuna
-VOTERS="-v 13"
-ESTIMATORS="-e 200"
+LR="-lr 0.09" # 0.0945 from optuna
+VOTERS="-v 12"
+ESTIMATORS="-e 140"
 LABELFILE=${TISSUE}_${METHOD}.csv
 #LABELFILE=PanTissue_group_HELP.csv
 
--- notebooks/EG_prediction.py
+++ notebooks/EG_prediction.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import random
-from HELPpy.preprocess.loaders import feature_assemble_df
+from HELPpy.preprocess.loaders import load_features
+from sklearn.metrics import *
+from imblearn.metrics import specificity_score, geometric_mean_score
 import os,sys
 import argparse
 from joblib import Parallel, delayed
@@ -15,7 +17,6 @@ warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser(description='PLOS COMPBIO')
 parser.add_argument('-i', "--inputfile", dest='inputfile', metavar='<inputfile>', nargs="+", type=str, help='input attribute filename list', required=True)
-parser.add_argument('-c', "--chunks", dest='chunks', metavar='<chunks>', nargs="+", type=int, help='no of chunks for attribute filename list', required=False)
 parser.add_argument('-X', "--excludelabels", dest='excludelabels', metavar='<excludelabels>', nargs="+", default=[], help='labels to exclude (default NaN, values any list)', required=False)
 parser.add_argument('-L', "--labelname", dest='labelname', metavar='<labelname>',  type=str, help='label name (default label)', default='label', required=False)
 parser.add_argument('-l', "--labelfile", dest='labelfile', metavar='<labelfile>', type=str, help='label filename', required=True)
@@ -26,10 +27,12 @@ parser.add_argument('-f', "--folds", dest='folds', metavar='<folds>', type=int, 
 parser.add_argument('-j', "--jobs", dest='jobs', metavar='<jobs>', type=int, help='n. of parallel jobs (default: -1)' , default=-1, required=False)
 parser.add_argument('-B', "--batch", action='store_true', help='enable batch mode (no output)', required=False)
 parser.add_argument('-v', "--voters", dest='voters', metavar='<voters>', type=int, help='n. of voter predictors (default: 1 - one classifier)' , default=1, required=False)
+parser.add_argument('-pl', "--poslabel", dest='poslabel', metavar='<poslabel>', type=int, help='positive label (default: 1)' , default=1, required=False)
+parser.add_argument('-fx', "--fixna", action='store_true', default=False, help='enable fixing NaN (default disabled)', required=False)
+parser.add_argument('-c', "--constrm", action='store_true', default=False, help='enable constant variable removal (default disabled)', required=False)
+parser.add_argument('-n', "--normalize", dest='normalize', metavar='<normalize>',  type=str, help='normalization mode (default None)', choices=['max', 'std'], required=False)
 parser.add_argument('-e', "--estimators", dest='estimators', metavar='<voters>', type=int, help='n. of estimators (default: 100)' , default=100, required=False)
 parser.add_argument('-lr', "--lr", dest='lr', metavar='<learningrate>', type=float, help='learning rate (default: 0.1)' , default=0.1, required=False)
-parser.add_argument('-fx', "--fixna", action='store_true', default=False, help='enable fixing NaN (default disabled)', required=False)
-parser.add_argument('-n', "--normalize", dest='normalize', metavar='<normalize>',  type=str, help='normalization mode (default None)', choices=['max', 'std'], required=False)
 parser.add_argument('-o', "--outfile", dest='outfile', metavar='<outfile>', help='output file for performance measures sumup', type=str, required=False)
 parser.add_argument('-s', "--scorefile", dest='scorefile', metavar='<scorefile>', type=str, help='output file reporting all measurements', required=False)
 parser.add_argument('-p', "--predfile", dest='predfile', metavar='<predfile>', type=str, help='output file reporting predictions', required=False)
@@ -54,6 +57,15 @@ def set_seed(seed=1):
     random.seed(seed)
     np.random.seed(seed)
 
+myscorer = { 'ROC-AUC':      (roc_auc_score, {'multi_class':'ovr', 'average':'macro'}),
+             'Accuracy'  :   (accuracy_score , {}),
+             'BA'  :         (balanced_accuracy_score , {}),
+             'G-mean'  :     (geometric_mean_score , {'pos_label': args.poslabel}),
+             'Specificity' : (specificity_score, {'pos_label': args.poslabel}),
+             'Sensitivity' : (recall_score   , {'pos_label': args.poslabel}),
+             'MCC' :         (matthews_corrcoef, {}), 
+            }
+
 clf = sveLGBM(n_voters=args.voters, n_estimators=args.estimators, learning_rate=args.lr,
                             n_jobs=args.jobs, 
                             random_state=args.seed, 
@@ -61,24 +73,10 @@ clf = sveLGBM(n_voters=args.voters, n_estimators=args.estimators, learning_rate=
 set_seed(1)
 label_file = args.labelfile
 label_name = args.labelname
-features = []
-if args.chunks is not None:                  # load files by chunks
-   assert len(args.chunks) == len(args.inputfile), "Chunk list must be same size of input list!" 
-   for attrfile,nchunks in zip(args.inputfile,args.chunks):
-      fixna = args.fixna
-      normalization = False if args.normalize is None else args.normalize
-      if 'Emb' in os.path.basename(attrfile):
-         features += [{'fname': attrfile, 'fixna' : False, 'normalize': None, 'nchunks': nchunks}]  # no normalization for embedding
-      else:
-         features += [{'fname': attrfile, 'fixna' : fixna, 'normalize': normalization, 'nchunks': nchunks}]
-else:                                         # no kile is are 
-   for attrfile in args.inputfile:
-      fixna = args.fixna
-      normalization = False if args.normalize is None else args.normalize
-      if 'Emb' in os.path.basename(attrfile):
-         features += [{'fname': attrfile, 'fixna' : False, 'normalize': None}]  # no normalization for embedding
-      else:
-         features += [{'fname': attrfile, 'fixna' : fixna, 'normalize': normalization}]
+filenames = []
+fixnans = []
+normalizes = []
+constrms = []
       
 df_lab = pd.read_csv(label_file, index_col=0)
 # exclude labels
@@ -91,21 +89,36 @@ for key,newkey in label_aliases.items():
     if key in exlabels:
         print(f'- replacing label {key} with {newkey}')
         df_lab = df_lab.replace(key, newkey)
-df_X, df_y = feature_assemble_df(df_lab, features=features, seed=1, verbose=verbose)
+for attrfile in args.inputfile:
+   fixna = args.fixna
+   constrm = args.constrm
+   normalization = False if args.normalize is None else args.normalize
+   if 'Emb' in os.path.basename(attrfile):
+      fixnans += [False]  # no fixnan for embedding
+      normalizes += [False]  # no normalization for embedding
+      constrms += [False]  # no reove constants for embedding
+   else:
+      fixnans += [fixna]  # no fixnan for embedding
+      normalizes += [normalization]  # no normalization for embedding
+      constrms += [constrm]  # no reove constants for embedding
+
+df_X = load_features(filenames=args.inputfile, fixnans=fixnans, normalizes=normalizes, constrms=constrms, verbose=verbose)
+idxs = np.intersect1d(df_lab.index.values, df_X.index.values)
+df_X = df_X.loc[idxs]
+df_lab = df_lab.loc[idxs]
 
 def classify(nfolds, repeat, jobs, verbose):
   if jobs == 1:
     print(f'Running seq on 1 cpu...')
-    result = [k_fold_cv(df_X, df_y, clf, n_splits=nfolds, seed=rseed, show_progress=True, verbose=verbose) for rseed in range(repeat)]
+    result = [skfold_cv(df_X, df_lab, clf, n_splits=nfolds, seed=rseed, show_progress=True, verbose=verbose, scorer=myscorer) for rseed in range(repeat)]
   else:
     if jobs == -1:
         print(f'Running par on {os.cpu_count()} cpus...')
     else:
         print(f'Running par on {jobs} cpus...')
-    result = Parallel(n_jobs=jobs)(delayed(skfold_cv)(df_X, df_y, clf, n_splits=nfolds, seed=rseed, show_progress=True, verbose=verbose) for rseed in range(repeat))
+    result = Parallel(n_jobs=jobs)(delayed(skfold_cv)(df_X, df_lab, clf, n_splits=nfolds, seed=rseed, show_progress=True, verbose=verbose, scorer=myscorer) for rseed in range(repeat))
   return result
 
-columns_names = ["ROC-AUC", "Accuracy","BA", "Sensitivity", "Specificity","MCC", 'CM']
 scores = pd.DataFrame()
 preds = pd.DataFrame()
 out = classify(args.folds, args.repeat, args.jobs, verbose)
@@ -121,14 +134,15 @@ if args.predfile is not None:
    preds.to_csv(args.predfile, index=False)
 else:
    print(preds)
-df_scores = pd.DataFrame([f'{val:.4f}±{err:.4f}' for val, err in zip(scores.loc[:, scores.columns != "CM"].mean(axis=0).values,
-                          scores.loc[:, scores.columns != "CM"].std(axis=0))] + [(scores[['CM']].sum()/args.repeat).values[0].tolist()],
+
+df_scores = pd.DataFrame([f'{val:.4f}±{err:.4f}' for val, err in zip(scores.loc[:, scores.columns != "cm"].mean(axis=0).values,
+                          scores.loc[:, scores.columns != "cm"].std(axis=0))] + [(scores[['cm']].sum()/args.repeat).values[0].tolist()],
                           columns=['measure'], index=scores.columns)
 import sys
-distrib = np.unique(df_y[label_name].values, return_counts=True)
+distrib = np.unique(df_lab[label_name].values, return_counts=True)
 ofile = sys.stdout if args.outfile is None else open(args.outfile, "a")
 ofile.write(f'METHOD: {type(clf).__name__}\n')
-ofile.write(f'PARAMS: {clf.get_params()}\n')
+ofile.write(f'PARAMS: {clf.get_params()} {clf.base_estimator.get_params()}\n')
 ofile.write(f'INPUT: {" ".join(str(os.path.basename(x)) for x in args.inputfile)}\n')
 ofile.write(f'LABEL: {os.path.basename(args.labelfile)} ({distrib[0][0]}:{distrib[1][0]},{distrib[0][1]}:{distrib[1][1]})\n')
 ofile.write(tabulate(df_scores, headers='keys', tablefmt='psql') + '\n')
